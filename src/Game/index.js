343ccd86caf54c4e47c047e2bb1d99c71f4e6d90
Peter Gyory <ptgyory@gmail.com>
2019-02-11 07:05:10 -0700

    Add assignment.md

--- src/Game/index.js
+++ src/Game/index.js
@@ -8,8 +8,8 @@ let yPos = 300;
 function create () {
   cursors = this.input.keyboard.createCursorKeys();
   graphics = this.add.graphics({
-    fillStyle: { color: 0xeeeeee},
-    lineStyle: { width: 3, color: 0xeeeeee},
+    fillStyle: { color: 0xeeeeee },
+    lineStyle: { width: 3, color: 0xeeeeee },
   });
 }
 
